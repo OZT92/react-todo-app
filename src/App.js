c6360e71955baf5e2909d3213ea97b9ad652489c
Yusa Ozturk <ozturkyusa@gmail.com>
2022-12-19 10:33:18 +0300

    sprint 02 is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Footer from "./components/ui/Footer.component";
+import Header from "./components/ui/Header.component";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+
+  const clickHandler = () => {
+    if (value === "") {
+      alert("Please enter something");
+      return;
+    }
+    // set list(value)
+    const tempList = [...list];
+    tempList.push(value);
+    setList(tempList);
+    setValue("");
+  };
+
+  const handleCleaerAll = () => {
+    setList([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        {/* heading */}
+        <h2>
+          Todo App{" "}
+          <button className="ml-6 btn-clear-all" onClick={handleCleaerAll}>
+            Clear All
+          </button>
+        </h2>
+
+        {/* create new todo */}
+        <div className="create-section">
+          <label>Todo</label>
+          <input
+            type="text"
+            className="input"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button className="button" onClick={clickHandler}>
+            Save
+          </button>
+        </div>
+
+        {/* show our todo list */}
+        <div className="todo-list">
+          {list.length > 0 ? (
+            list.map((item, index) => (
+              <div key={index} className="todo-item">
+                {item}
+              </div>
+            ))
+          ) : (
+            <h1>No Todos</h1>
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
